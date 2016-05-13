b0baee71369add6a848bf96989faf692ea1f2f03
Emanuel Calvo <3manuek@esdebian.org>
2016-05-13 11:33:14 -0300

    Update sar_collector.sh

--- system/sar_collector.sh
+++ system/sar_collector.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# Sar to be processed in ksar
+
+DT=$(ls /var/log/sa/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sa\/sa/ /g') 
+ > /tmp/sar-$(hostname)-multiple.txt for i in $DT; do 
+  LC_ALL=C sar -A -f /var/log/sa/sa$i >> /tmp/sar-$(hostname)-multiple.txt 
+done
 
