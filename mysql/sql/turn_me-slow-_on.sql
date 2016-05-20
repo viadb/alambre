58608c7d3963d091fd8a8bd6f5b44be1caaf5a5d
Emanuel Calvo <3manuek@esdebian.org>
2016-05-20 16:59:48 -0300

    Create turn_me-slow-_on.sql

---
+++ mysql/sql/turn_me-slow-_on.sql
@@ -0,0 +1,11 @@
+
+SET GLOBAL slow_query_log_file='/mysql_logs/mysql-slow-test.log';
+SET GLOBAL long_query_time=0;
+SET GLOBAL slow_query_log_use_global_control='all';
+FLUSH LOGS;
+
+
+SET GLOBAL slow_query_log_file='/mysql_logs/mysql-slow.log';
+SET GLOBAL long_query_time=3;
+SET GLOBAL slow_query_log_use_global_control='';
+FLUSH LOGS;
