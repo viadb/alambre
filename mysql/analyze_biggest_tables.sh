9e8ba54b9532d88054ab180108c7f3101214d4dd
Emanuel Calvo <3manuek@esdebian.org>
2016-06-02 20:49:14 -0300

    Update analyze_biggest_tables.sh

--- mysql/analyze_biggest_tables.sh
+++ mysql/analyze_biggest_tables.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
-
+NTABLES=40
 LOG=analyze_tables.log
 DBLIST=analyze_tables_dblist
 BIGTABLES=analyze_tables_tables
 source .analyze_tables
-HOSTDEST="-h10.10.10.10"
+#HOSTDEST="-h10.183.160.xxx"
+HOSTDEST=""
 
 function executeQuery() {
   mysql -BN $HOSTDEST -uroot -p$pw -e "$1" "$2"
 }
 
 function analyze() {
-  mysqlcheck  $HOSTDEST -uroot -p$pw --analyze "$1" "$2"
+  mysqlcheck $HOSTDEST -uroot -p$pw --analyze "$1" "$2"
 }
-
 
 echo "###" >> $LOG
 echo "Start $(date)" >> $LOG
 executeQuery "show databases" \
   | egrep -v "information_schema|mysql|performance_schema|test" > $DBLIST
 
-for db in $(<$DBLIST)
-do
-  executeQuery "SELECT CONCAT(table_schema, '.', table_name) FROM   information_schema.TABLES ORDER  BY data_length + index_length DESC LIMIT  10" $db > $BIGTABLES
+executeQuery "SELECT CONCAT(table_schema, ' ', table_name) FROM information_schema.TABLES ORDER  BY data_length + index_length DESC LIMIT  $NTABLES" $db > $BIGTABLES
 
-  for tab in $(<$BIGTABLES)
-  do
+while read db tab; do
     echo "Analyzing $(date): $db $tab" >> $LOG
     analyze $db $tab >> $LOG
-  done
-done
+done < $BIGTABLES
 
 echo "Done  $(date)" >> $LOG
 
