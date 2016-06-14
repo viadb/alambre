3dd7ea480ca4bd27ce5a5d38418c1f91f9afbc1e
Emanuel Calvo <3manuek@esdebian.org>
2016-06-14 19:57:47 -0300

    Update and rename get_SHOWs_and_execute_in_mysql.sh to get_SHOWs_from_ptquerydigest_and_execute_in_mysql.sh

--- mysql/get_SHOWs_from_ptquerydigest_and_execute_in_mysql.sh
+++ mysql/get_SHOWs_from_ptquerydigest_and_execute_in_mysql.sh
@@ -3,6 +3,8 @@
 # $2 password
 # $3 output
 
+# Input is the output of pt-query-digest
+
 egrep "SHOW.[TABLE|CREATE].*" $1 \
 | sed 's/^#\s*//' | sed 's/\\/\\\\/g' | sort | uniq \
 | sed "s/'/\\\'/g" | xargs -i mysql --user=admin --password=$2 -e {} > $3
