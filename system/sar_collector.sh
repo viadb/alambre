aa8f244fdaca3fb25db08feb6f2a65ec64000f4c
Emanuel Calvo <3manuek@esdebian.org>
2016-08-22 16:56:27 -0300

    gzip and fix

--- system/sar_collector.sh
+++ system/sar_collector.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Sar to be processed in ksar
+# Some systems have /var/log/sysstat/
 
 DT=$(ls /var/log/sa/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sa\/sa/ /g') 
- > /tmp/sar-$(hostname)-multiple.txt for i in $DT; do 
+for i in $DT; do 
   LC_ALL=C sar -A -f /var/log/sa/sa$i >> /tmp/sar-$(hostname)-multiple.txt 
-done
+done | gzip -c  > /tmp/sar-$(hostname)-multiple.txt
 
