3d199ae40ca0247b5d976150701ec2177987a537
Emanuel Calvo <3manuek@gmail.com>
2017-01-26 19:27:16 -0300

    Create parse_binlog_incrementally.sh

---
+++ mysql/binlogTools/parse_binlog_incrementally.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# $1 relay or binlog file
+
+DUMPFILE=_proc_$(echo $1 | grep -o -P "\K[[:digit:]]+$")
+
+if [ -f $1 ]
+then
+  [ -f $DUMPFILE ] && \
+  mysqlbinlog --base64-output='decode-rows' -vvv --start-position=$( tac _proc_1001 | head -1000 | grep -m1 -oP '^# at \K[[:digit:]]+') laslsql10p-relay-bin.001001 >> $DUMPFILE \
+  || \
+  mysqlbinlog --base64-output='decode-rows' -vvv laslsql10p-relay-bin.001001 >> $DUMPFILE
+else
+  echo "Binlog/Relay do not exists."
+  exit 1
+fi
+
+exit 0 
