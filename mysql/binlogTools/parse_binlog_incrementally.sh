dbd6726139f7dd998973725a8823ab1269b94b76
Emanuel Calvo <3manuek@gmail.com>
2017-01-26 19:28:11 -0300

    Update parse_binlog_incrementally.sh

--- mysql/binlogTools/parse_binlog_incrementally.sh
+++ mysql/binlogTools/parse_binlog_incrementally.sh
@@ -7,7 +7,7 @@ DUMPFILE=_proc_$(echo $1 | grep -o -P "\K[[:digit:]]+$")
 if [ -f $1 ]
 then
   [ -f $DUMPFILE ] && \
-  mysqlbinlog --base64-output='decode-rows' -vvv --start-position=$( tac _proc_1001 | head -1000 | grep -m1 -oP '^# at \K[[:digit:]]+') laslsql10p-relay-bin.001001 >> $DUMPFILE \
+  mysqlbinlog --base64-output='decode-rows' -vvv --start-position=$( tac $DUMPFILE | head -1000 | grep -m1 -oP '^# at \K[[:digit:]]+') laslsql10p-relay-bin.001001 >> $DUMPFILE \
   || \
   mysqlbinlog --base64-output='decode-rows' -vvv laslsql10p-relay-bin.001001 >> $DUMPFILE
 else
