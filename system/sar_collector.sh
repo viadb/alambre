ef5443ad4e36c5f6c720d6a417de723bbbed734f
Emanuel Calvo <3manuek@gmail.com>
2017-08-22 18:52:27 -0300

    Update sar_collector.sh

--- system/sar_collector.sh
+++ system/sar_collector.sh
@@ -4,13 +4,15 @@
 # Some systems have /var/log/sysstat/
 
 # For RHEL variants
-DT=$(ls /var/log/sa/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sa\/sa/ /g') 
+DT=$(ls /var/log/sa/sa[0-9][0-9]) 
 for i in $DT; do 
-  LC_ALL=C sar -A -f /var/log/sa/sa$i # >> /tmp/sar-$(hostname)-multiple.txt 
+  LC_ALL=C sar -A -f $i
 done | gzip -c  > /tmp/sar-$(hostname)-multiple.txt.gz
 
 # For debian/ubuntu
-DT=$(ls /var/log/sysstat/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sysstat\/sa/ /g') 
+DT=$(ls /var/log/sysstat/sa[0-9][0-9]) 
 for i in $DT; do 
-  LC_ALL=C sar -A -f /var/log/sysstat/sa$i # >> /tmp/sar-$(hostname)-multiple.txt 
+  LC_ALL=C sar -A -f $i  ##  >> /tmp/sar-$(hostname)-multiple.txt 
 done | gzip -c  > /tmp/sar-$(hostname)-multiple.txt.gz
+
+# Some shell can need parsing ls output (check previous commits) 
